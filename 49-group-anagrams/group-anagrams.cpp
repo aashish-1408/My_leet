aefc96adbf6f553cc995a3e9fd3fe2907b059e35
aashish-1408 <aashish.peak@gmail.com>
2026-09-21 11:20:51 +0530

    Time: 22 ms (32.07%) | Memory: 26.1 MB (53.84%) - LeetSync

---
+++ 49-group-anagrams/group-anagrams.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    vector<vector<string>> groupAnagrams(vector<string>& strs) {
+        unordered_map <string,vector<string>> m;
+
+        for(auto a:strs){
+            string k=a;
+            sort(k.begin(),k.end());
+                m[k].push_back(a);
+         }
+     vector<vector<string>> ans;
+     for(auto it:m){
+         ans.push_back(it.second);
+          }
+    return ans;
+    }
+};
